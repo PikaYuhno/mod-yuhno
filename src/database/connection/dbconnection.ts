6ea61815d35df6af1bc9ba0350a4669f5fc704f0
PikaYuhno <muazahmed019@gmail.com>
2020-07-15 21:54:56 +0200

    changed host name

--- src/database/connection/dbconnection.ts
+++ src/database/connection/dbconnection.ts
@@ -1,11 +1,12 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 export const createConnection = () => {
-    let conn = new Sequelize('postgres', 'postgres', 'postgres', {
-        host: 'localhost',
-        dialect: 'postgres',
-        logging: false
+    let conn = new Sequelize("postgres", "postgres", "postgres", {
+        host: "0.0.0.0",
+        dialect: "postgres",
+        logging: false,
     });
     return conn;
-}
+};
 export const sequelize = createConnection();
+

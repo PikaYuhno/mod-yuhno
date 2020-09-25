591b58e3780effb48f76ac63bc7b16e18580e829
PikaYuhno <muazahmed019@gmail.com>
2020-09-25 19:31:14 +0200

    deleted dist

--- dist/events/ready.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("../utils/");
-exports.default = async (client) => {
-    await utils_1.verifyDatabase(client);
-    await utils_1.loadConfiguration(client);
-    console.log(`Logged in as ${client.user.tag}`);
-};
